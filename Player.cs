96c329645b918ed9e53507dbff67fe9f1e786a86
opilane <opilane@HP-CZC2349HTY>
2023-09-15 09:56:47 +0300

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jalgpall
+{
+    public class Player
+    {
+        //поля
+        public string Name { get; } //имя игрока 
+        public double X { get; private set; } //х координата
+        public double Y { get; private set; } //у координата
+        private double _vx, _vy; //расстояние игрока и мяча
+        public Team? Team { get; set; } = null; //команда где играет игрок
+
+        private const double MaxSpeed = 5; //максимальная скрость игрока
+        private const double MaxKickSpeed = 25; //максимальная скорость удара
+        private const double BallKickDistance = 10; //дистанция удара меча
+
+        private Random _random = new Random(); //рандомное число
+
+        //конструкторы
+        public Player(string name)   //конструктор создания игрока используя только имя
+        {
+            Name = name;
+        }
+
+        public Player(string name, double x, double y, Team team) // конструктор создания игрока используя имя, координаты и команду
+        {
+            Name = name;
+            X = x;
+            Y = y;
+            Team = team;
+        }
+
+        public void SetPosition(double x, double y)  //установка координатов игрока
+        {
+            X = x;
+            Y = y;
+        }
+
+        public (double, double) GetAbsolutePosition()  //получение абсолютной позиции
+        {
+            return Team!.Game.GetPositionForTeam(Team, X, Y);
+        }
+
+        public double GetDistanceToBall() //получение дистанции до мяча
+        {
+            var ballPosition = Team!.GetBallPosition();
+            var dx = ballPosition.Item1 - X;
+            var dy = ballPosition.Item2 - Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        public void MoveTowardsBall() //передвижение игрока к мячу
+        {
+            var ballPosition = Team!.GetBallPosition();
+            var dx = ballPosition.Item1 - X;
+            var dy = ballPosition.Item2 - Y;
+            var ratio = Math.Sqrt(dx * dx + dy * dy) / MaxSpeed;
+            _vx = dx / ratio;
+            _vy = dy / ratio;
+        }
+
+        public void Move() //передвижение игрока
+        {
+            if (Team.GetClosestPlayerToBall() != this) //если не определенно
+            {
+                _vx = 0;
+                _vy = 0;
+            }
+
+            if (GetDistanceToBall() < BallKickDistance) //если дистанция мяча меньше дистанции удара мяча
+            {
+                Team.SetBallSpeed(
+                    MaxKickSpeed * _random.NextDouble(),
+                    MaxKickSpeed * (_random.NextDouble() - 0.5)
+                    ); //устанавливаем скорость мяча
+            }
+
+            var newX = X + _vx; //установка  новых координат и абсолютной позиции
+            var newY = Y + _vy;
+            var newAbsolutePosition = Team.Game.GetPositionForTeam(Team, newX, newY);
+            if (Team.Game.Stadium.IsIn(newAbsolutePosition.Item1, newAbsolutePosition.Item2)) //проверяем находится ли мяч на поле или нет и устанавливаем координаты
+            {
+                X = newX;
+                Y = newY;
+            }
+            else
+            {
+                _vx = _vy = 0;
+            }
+        }
+    }
+}
---
+++ Stadium.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jalgpall
+{
+    public class Stadium
+    {
+        public Stadium(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public bool IsIn(double x, double y) //возвращает правду или ложь в зависимости от того находится мяч на поле или вне поля
+        {
+            return x >= 0 && x < Width && y >= 0 && y < Height;
+        }
+    }
+}
